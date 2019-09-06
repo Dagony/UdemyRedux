fac330ea78d00e8fcd652a12886f0a11fcddca64
mark <dagony@gmail.com>
2019-09-06 10:30:31 +0200

    Does this fix the npm install killed?

--- src/App.test.js
+++ src/App.test.js
@@ -1,9 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
-import { Provider } from 'react-redux';
 
 it('renders without crashing', () => {
   const div = document.createElement('div');
-  ReactDOM.render(<Provider><App /></Provider>, div);
+  ReactDOM.render(<App />, div);
 });
