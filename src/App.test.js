330120734011581cfd7456f032fe9bb10f3ae681
mark <dagony@gmail.com>
2019-09-18 10:59:21 +0200

    Fix App.test.js by adding redux with a store with the reducers and a provider

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,20 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import {createStore,combineReducers} from 'redux';
+import {Provider} from 'react-redux';
 import App from './App';
+
+import counterReducer from "./store/reducer";
+import resultReducer from "./store/reducer";
+
+const rootReducer = combineReducers({
+  ctr: counterReducer,
+  res: resultReducer
+});
+
+const store = createStore(rootReducer);
 
 it('renders without crashing', () => {
   const div = document.createElement('div');
-  ReactDOM.render(<App />, div);
+  ReactDOM.render(<Provider store={store}><App /></Provider>, div);
 });
